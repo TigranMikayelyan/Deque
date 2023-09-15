efc007614312d479c80deb9ee3b627e0fb10f1ed
TigranMikayelyan <112478852+TigranMikayelyan@users.noreply.github.com>
2023-09-15 17:23:16 +0400

    Deque

--- main.cpp
+++ main.cpp
@@ -2,22 +2,5 @@
 
 int main()
 {
-    Deque<int> md;
-    md.push_back(1);
-    md.push_back(2);
-    md.push_back(3);
-    md.print();
-    md.pop_back();
-    md.pop_back();
-    md.print();
-    md.push_front(77);
-    md.push_front(99);
-    md.print();
-    std::cout << "Size = " << md.size() << std::endl;
-    md.clear();
-    std::cout << "Size = " << md.size() << std::endl;
-    md.print();
-    std::cout << std::boolalpha << md.empty() << std::endl;
-    md.push_back(3);
-    std::cout << std::boolalpha << md.empty() << std::endl;
+    return 0;
 }
